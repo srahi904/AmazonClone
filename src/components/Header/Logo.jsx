44ff4abfebb52ad5e111e5bce661794cccf17c4e
Rahi <63035780+srahi904@users.noreply.github.com>
2025-04-24 22:49:57 +0530

    Update Logo.jsx
    logo updated

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -7,7 +7,7 @@ const Logo = () => {
   return (
     <Link to="/" className="mr-4">
       <img
-        src="https://assets.aboutamazon.com/2e/d7/ac71f1f344c39f8949f48fc89e71/amazon-logo-squid-ink-smile-orange.png"
+        src="https://www.pngplay.com/wp-content/uploads/3/White-Amazon-Logo-PNG-HD-Quality.png"
         alt="Amazon Logo"
         className="h-8 w-auto cursor-pointer "
       />
